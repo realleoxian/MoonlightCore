22f7125675e15ecb108eff6a1d8757eed0753242
LeoWgc <leonardojmarinellib09@gmail.com>
2025-08-02 21:09:23 -0400

    Changes on fabric

--- Fabric/src/main/java/de/leowgc/moonlightcore/fabric/api/MoonlightCoreInitializer.java
+++ Fabric/src/main/java/de/leowgc/moonlightcore/fabric/api/MoonlightCoreInitializer.java
@@ -2,8 +2,6 @@ package de.leowgc.moonlightcore.fabric.api;
 
 public interface MoonlightCoreInitializer {
 
-    default void onServerInitialized() {}
-
     void onInitialize();
 
     default void onClientInitialized() {}
