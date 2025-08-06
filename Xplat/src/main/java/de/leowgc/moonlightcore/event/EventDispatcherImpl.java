864e5e713d881c39996edea24bdc6c2ebe646ab6
LeoWgc <leonardojmarinellib09@gmail.com>
2025-08-06 12:52:40 -0400

    Bump to 4.1.2 🚀

--- Xplat/src/main/java/de/leowgc/moonlightcore/event/EventDispatcherImpl.java
+++ Xplat/src/main/java/de/leowgc/moonlightcore/event/EventDispatcherImpl.java
@@ -2,6 +2,7 @@ package de.leowgc.moonlightcore.event;
 
 import de.leowgc.moonlightcore.api.event.Event;
 import de.leowgc.moonlightcore.api.event.EventDispatcher;
+import de.leowgc.moonlightcore.api.event.EventPriority;
 
 import java.util.function.Consumer;
 
@@ -9,12 +10,13 @@ public enum EventDispatcherImpl implements EventDispatcher {
     INSTANCE
     ;
 
-
     @Override
     public <T> void fire(Event<T> event, Consumer<T> output) {
         try {
-            for(T listener : ((EventImpl<T>) event).listeners().stream().map((listener) -> listener.listener).toList()) {
-                output.accept(listener);
+            for(EventPriority priority : EventPriority.values()) {
+                for(T listener : ((EventImpl<T>) event).listeners(priority)) {
+                    output.accept(listener);
+                }
             }
         } catch (Exception exception) {
             throw new RuntimeException(exception);
--- Xplat/src/main/java/de/leowgc/moonlightcore/event/EventImpl.java
+++ Xplat/src/main/java/de/leowgc/moonlightcore/event/EventImpl.java
@@ -3,57 +3,60 @@ package de.leowgc.moonlightcore.event;
 import com.google.common.collect.ImmutableList;
 import de.leowgc.moonlightcore.api.event.Event;
 import de.leowgc.moonlightcore.api.event.EventPriority;
-import de.leowgc.moonlightcore.api.util.SortedLinkedList;
 
-import java.util.Objects;
+import java.util.*;
 
 public final class EventImpl<T> implements Event<T> {
-    private final SortedLinkedList<Listener<T>> listeners;
+    private final Map<EventPriority, List<T>> listeners;
 
     public EventImpl() {
-        this.listeners = new SortedLinkedList<>((a, b) -> b.priority.ordinal() - a.priority.ordinal());
+        this.listeners = new EnumMap<>(EventPriority.class);
+
+        for(EventPriority priority : EventPriority.values()) {
+            this.listeners.put(priority, new ArrayList<>());
+        }
     }
 
     @Override
     public void subscribe(EventPriority priority, T listener) {
-        if(!this.listeners.contains(listener)) {
-            this.listeners.add(new Listener<>(priority, listener));
+        if(!this.listeners.get(priority).contains(listener)) {
+            this.listeners.get(priority).add(listener);
         }
     }
 
-    public ImmutableList<Listener<T>> listeners() {
-        return ImmutableList.copyOf(this.listeners);
+    public ImmutableList<T> listeners(EventPriority priority) {
+        return ImmutableList.copyOf(this.listeners.get(priority));
     }
 
-    public static class Listener<T> {
-        private final EventPriority priority;
-        final T listener;
-
-        private Listener(EventPriority priority, T listener) {
-            this.priority  =priority;
-            this.listener = listener;
-        }
-
-        @Override
-        public boolean equals(Object obj) {
-            if(this == obj) return true;
-            if(obj instanceof Listener<?> that) {
-                return Objects.equals(this.listener, that.listener);
-            } else if (obj != null) {
-                return obj.equals(this.listener);
-            }
-
-            return false;
-        }
-
-        @Override
-        public int hashCode() {
-            return Objects.hash(this.listener);
-        }
-
-        @Override
-        public String toString() {
-            return this.listener.toString() + " - " + this.priority;
-        }
-    }
+//    public static class Listener<T> {
+//        private final EventPriority priority;
+//        final T listener;
+//
+//        private Listener(EventPriority priority, T listener) {
+//            this.priority  =priority;
+//            this.listener = listener;
+//        }
+//
+//        @Override
+//        public boolean equals(Object obj) {
+//            if(this == obj) return true;
+//            if(obj instanceof Listener<?> that) {
+//                return Objects.equals(this.listener, that.listener);
+//            } else if (obj != null) {
+//                return obj.equals(this.listener);
+//            }
+//
+//            return false;
+//        }
+//
+//        @Override
+//        public int hashCode() {
+//            return Objects.hash(this.listener);
+//        }
+//
+//        @Override
+//        public String toString() {
+//            return this.listener.toString() + " - " + this.priority;
+//        }
+//    }
 }
