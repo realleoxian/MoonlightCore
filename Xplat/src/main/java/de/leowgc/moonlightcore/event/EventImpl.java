d7ca0d08563d951d58d6c6b61a081a1fac9ebc5e
LeoWgc <leonardojmarinellib09@gmail.com>
2025-08-18 11:11:36 -0400

    Undo 4.1.4 changes

--- Xplat/src/main/java/de/leowgc/moonlightcore/event/EventImpl.java
+++ Xplat/src/main/java/de/leowgc/moonlightcore/event/EventImpl.java
@@ -3,24 +3,55 @@ package de.leowgc.moonlightcore.event;
 import com.google.common.collect.ImmutableList;
 import de.leowgc.moonlightcore.api.event.Event;
 import de.leowgc.moonlightcore.api.event.EventPriority;
+import de.leowgc.moonlightcore.util.SortedLinkedList;
 
 import java.util.*;
 
 public final class EventImpl<T> implements Event<T> {
-    private final Map<EventPriority, List<T>> listeners = new HashMap<>();
+    private final SortedLinkedList<Listener<T>> listeners;
 
     public EventImpl() {
-        for(EventPriority priority : EventPriority.values()) {
-            this.listeners.put(priority, new ArrayList<>());
-        }
+        this.listeners = new SortedLinkedList<>((a, b) -> b.priority.ordinal() - a.priority.ordinal());
     }
 
     @Override
     public void subscribe(EventPriority priority, T listener) {
-        this.listeners.get(priority).add(listener);
+        this.listeners.add(new Listener<>(priority, listener));
     }
 
-    public List<T> listeners(EventPriority priority) {
-        return ImmutableList.copyOf(this.listeners.get(priority));
+    public List<T> listeners() {
+        return ImmutableList.copyOf(this.listeners).stream().map((listener) -> listener.listener).toList();
+    }
+
+    public static class Listener<T> {
+        private final EventPriority priority;
+        final T listener;
+
+        private Listener(EventPriority priority, T listener) {
+            this.priority  =priority;
+            this.listener = listener;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if(this == obj) return true;
+            if(obj instanceof Listener<?> that) {
+                return Objects.equals(this.listener, that.listener);
+            } else if (obj != null) {
+                return obj.equals(this.listener);
+            }
+
+            return false;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(this.listener);
+        }
+
+        @Override
+        public String toString() {
+            return this.listener.toString() + " - " + this.priority;
+        }
     }
 }
--- changelog/4.1.4/changelog.md
+++
@@ -1,3 +0,0 @@
-# Moonlight Core | 4.1.4
-
-Undo previous event dispatch changes
