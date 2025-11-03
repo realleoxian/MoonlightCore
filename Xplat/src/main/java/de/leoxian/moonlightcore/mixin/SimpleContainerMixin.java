4c30a5faf149eb248d675081fa591175a7975673
leoxian <leonardojmarinellib09@gmail.com>
2025-11-03 23:12:16 +0400

    Fix crash with fabric

--- Xplat/src/main/java/de/leoxian/moonlightcore/mixin/SimpleContainerMixin.java
+++ Xplat/src/main/java/de/leoxian/moonlightcore/mixin/SimpleContainerMixin.java
@@ -7,18 +7,19 @@ import net.minecraft.world.item.ItemStack;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(SimpleContainer.class)
 public class SimpleContainerMixin implements SpecialLogicInventory {
     @Unique
     private boolean mlcore_suppressSpecialLogic = false;
 
-    @Redirect(method = "setItem(ILnet/minecraft/world/item/ItemStack;)V",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/world/SimpleContainer;setChanged()V"))
-    public void mlcore_redirectSetChanged(SimpleContainer self) {
-        if(!this.mlcore_suppressSpecialLogic) {
-            self.setChanged();
+    @Inject(method = "setItem(ILnet/minecraft/world/item/ItemStack;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/SimpleContainer;setChanged()V"), cancellable = true)
+    public void mlcore_redirectSetChanged(int index, ItemStack stack, CallbackInfo ci) {
+        if(this.mlcore_suppressSpecialLogic) {
+            ci.cancel();
         }
     }
 
