2856df197ee4fd4cec14e095e0cc5c03d25a9671
leoxian <leonardojmarinellib09@gmail.com>
2025-11-20 18:58:16 -0400

    Rename NonnullType and NullableType

--- Xplat/src/main/java/de/leoxian/moonlightcore/util/nullness/Nonnull.java
+++ Xplat/src/main/java/de/leoxian/moonlightcore/util/nullness/Nonnull.java
@@ -1,14 +1,13 @@
 package de.leoxian.moonlightcore.util.nullness;
 
-import javax.annotation.Nonnull;
 import java.lang.annotation.*;
 
 /**
  * An alternative to {@link javax.annotation.Nonnull} which works with type parameters
  */
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
+@Target({ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Nonnull
-public @interface NonnullType {
+@javax.annotation.Nonnull
+public @interface Nonnull {
 }
--- Xplat/src/main/java/de/leoxian/moonlightcore/util/nullness/Nullable.java
+++ Xplat/src/main/java/de/leoxian/moonlightcore/util/nullness/Nullable.java
@@ -1,13 +1,12 @@
 package de.leoxian.moonlightcore.util.nullness;
 
-import javax.annotation.Nullable;
 import java.lang.annotation.*;
 
 /**
  * An alternative to {@link javax.annotation.Nullable} which works on type parameters
  */
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER, ElementType.TYPE_PARAMETER, ElementType.TYPE_USE})
+@Target({ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
-@Nullable
-public @interface NullableType {}
+@javax.annotation.Nullable
+public @interface Nullable {}
