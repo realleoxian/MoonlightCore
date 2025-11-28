399b316d06a77ef7614b4be7d930006f014b42b0
leoxian <leonardojmarinellib09@gmail.com>
2025-11-27 20:45:16 -0400

    Remove `abstract` from BaseMobEffect

--- Xplat/src/main/java/de/leoxian/moonlightcore/effect/BaseMobEffect.java
+++ Xplat/src/main/java/de/leoxian/moonlightcore/effect/BaseMobEffect.java
@@ -3,7 +3,7 @@ package de.leoxian.moonlightcore.effect;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 
-public abstract class BaseMobEffect extends MobEffect {
+public class BaseMobEffect extends MobEffect {
 
     public BaseMobEffect(MobEffectCategory category, int color) {
         super(category, color);
