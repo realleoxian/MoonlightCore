4f878d1d822027ebf99d1a29b91709adc1009f25
RealLeoxian <leonardojmarinellib09@gmail.com>
2026-03-18 13:10:45 -0400

    Update Fluid API

---
+++ Xplat/src/main/java/de/leoxian/moonlightcore/api/fluid/FluidConstants.java
@@ -0,0 +1,15 @@
+package de.leoxian.moonlightcore.api.fluid;
+
+public class FluidConstants {
+
+    public static final int BUCKET = 81000;
+    public static final int BOTTLE = 27000;
+    public static final int BOWL = 27000;
+    public static final int BLOCK = 81000;
+    public static final int INGOT = 9000;
+    public static final int NUGGET = 1000;
+    public static final int DROPLET = 1;
+
+    private FluidConstants() {}
+
+}
