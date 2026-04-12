33af5807fdcfbd4af0495037b056e01d82ef87d9
RealLeoxian <leonardojmarinellib09@gmail.com>
2026-04-12 18:37:27 -0400

    Add client colors

---
+++ Xplat/src/main/java/de/leoxian/moonlightcore/api/client/render/EntityRendererRegistrar.java
@@ -0,0 +1,9 @@
+package de.leoxian.moonlightcore.api.client.render;
+
+import net.minecraft.client.renderer.entity.EntityRendererProvider;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+
+public interface EntityRendererRegistrar {
+    <E extends Entity> void register(EntityType<E> entityType, EntityRendererProvider<E> provider);
+}
---
+++ Xplat/src/main/java/de/leoxian/moonlightcore/api/client/render/color/Tintable.java
@@ -0,0 +1,41 @@
+package de.leoxian.moonlightcore.api.client.render.color;
+
+import net.minecraft.client.color.block.BlockColor;
+import net.minecraft.client.color.item.ItemColor;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.level.BlockAndTintGetter;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import org.jetbrains.annotations.Nullable;
+
+public interface Tintable {
+    default int getColor(int idx) {
+        return -1;
+    }
+
+    default int getColor(int idx, ItemStack stack) {
+        return getColor(idx);
+    }
+
+    final class TintedItem implements ItemColor {
+        @Override
+        public int getColor(ItemStack itemStack, int i) {
+            return ((Tintable) itemStack.getItem()).getColor(i, itemStack);
+        }
+    }
+
+    final class TinterBlockItemImpl implements ItemColor {
+        @Override
+        public int getColor(ItemStack itemStack, int i) {
+            return ((Tintable) Block.byItem(itemStack.getItem())).getColor(i, itemStack);
+        }
+    }
+
+    final class TintedBlockImpl implements BlockColor {
+        @Override
+        public int getColor(BlockState blockState, @Nullable BlockAndTintGetter blockAndTintGetter, @Nullable BlockPos blockPos, int i) {
+            return ((Tintable) blockState.getBlock()).getColor(i);
+        }
+    }
+}
