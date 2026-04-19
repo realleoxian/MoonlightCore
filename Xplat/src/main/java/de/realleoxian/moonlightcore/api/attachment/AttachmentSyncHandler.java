b5f12d4bec84382342777c354c0f18820f40178d
RealLeoxian <leonardojmarinellib09@gmail.com>
2026-04-19 12:52:27 -0400

    Tf happened with formatting here

--- Xplat/src/main/java/de/realleoxian/moonlightcore/api/attachment/AttachmentSyncHandler.java
+++ Xplat/src/main/java/de/realleoxian/moonlightcore/api/attachment/AttachmentSyncHandler.java
@@ -3,14 +3,12 @@ package de.realleoxian.moonlightcore.api.attachment;
 import net.minecraft.network.FriendlyByteBuf;
 import net.minecraft.server.level.ServerPlayer;
 
-    public interface AttachmentSyncHandler<T> {
+public interface AttachmentSyncHandler<T> {
+    void encode(FriendlyByteBuf byteBuf, AttachmentType<T> type, T data);
 
-        void encode(FriendlyByteBuf byteBuf, AttachmentType<T> type, T data);
+    T decode(FriendlyByteBuf byteBuf, AttachmentType<T> type);
 
-        T decode(FriendlyByteBuf byteBuf, AttachmentType<T> type);
-
-        default boolean canSync(AttachmentHolder holder, ServerPlayer player) {
+    default boolean canSync(AttachmentHolder holder, ServerPlayer player) {
             return true;
         }
-
-    }
+}
