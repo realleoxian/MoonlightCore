8dca1370f2c1840c673cbbc748627b3851693d89
RealLeoxian <leonardojmarinellib09@gmail.com>
2026-06-15 11:30:11 -0400

    Add configuration completed client event

--- xplat/src/main/java/de/realleoxian/moonlightcore/api/client/event/ClientPlayerNetworkEvents.java
+++ xplat/src/main/java/de/realleoxian/moonlightcore/api/client/event/ClientPlayerNetworkEvents.java
@@ -4,12 +4,14 @@ import de.realleoxian.moonlightcore.api.event.Event;
 import de.realleoxian.moonlightcore.api.event.EventBase;
 import de.realleoxian.moonlightcore.api.network.PacketSender;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.multiplayer.ClientConfigurationPacketListenerImpl;
 import net.minecraft.client.multiplayer.ClientPacketListener;
 import org.jetbrains.annotations.ApiStatus;
 
 public final class ClientPlayerNetworkEvents {
     public static final Event<LoggedIn> LOGGED_IN = Event.create(LoggedIn.class);
     public static final Event<LoggedOut> LOGGED_OUT = Event.create(LoggedOut.class);
+    public static final Event<ConfigurationCompleted> CONFIGURATION_COMPLETED = Event.create(ConfigurationCompleted.class);
 
     private ClientPlayerNetworkEvents() {}
 
@@ -32,6 +34,16 @@ public final class ClientPlayerNetworkEvents {
 
         @ApiStatus.Internal
         public LoggedOut(ClientPacketListener networkHandler, Minecraft minecraft) {
+            this.networkHandler = networkHandler;
+            this.minecraft = minecraft;
+        }
+    }
+
+    public static final class ConfigurationCompleted extends EventBase {
+        public final ClientConfigurationPacketListenerImpl networkHandler;
+        public final Minecraft minecraft;
+
+        public ConfigurationCompleted(ClientConfigurationPacketListenerImpl networkHandler, Minecraft minecraft) {
             this.networkHandler = networkHandler;
             this.minecraft = minecraft;
         }
